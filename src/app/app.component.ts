905a69c4714e8433ccdccb348c378660c08807b8
Ferchop89 <fechopacheco@gmail.com>
2020-07-02 17:22:41 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,14 @@ Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at http://angular.io/license
 
+Monte proyecto e_titulos en servidor nemesis.
+Generé rutas, menus, usuario y todo lo necesario para agregar el modulo de tramitel.
+
+Agregué al proyecto ati un modulo de autenticacion para la administracion de horarios de citas.
+Agregué botones con animaciones para la navegación de los alumnos en el proyecto ATI.
+
+Envie cedulas electonicas a Profesiones
+Diseñe la arquitectura del modulo de citas
 
 
 
